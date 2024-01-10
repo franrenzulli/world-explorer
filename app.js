935bb2088c4666ff1e911fa7bb132699489bed73
m <85003859+mittahtb@users.noreply.github.com>
2024-01-10 19:48:36 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
-const express = require("express")
-const app = express()
 
-
-
-async function fetchFunction(url){    
-    try {
-        const response = await fetch(url);
-        const result = await response.text();
-        console.log(result);
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-module.exports = { fetchFunction }
